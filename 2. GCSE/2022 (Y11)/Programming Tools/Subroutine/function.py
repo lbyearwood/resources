93eea3b9daac739138b683520e4abdadb4c51b86
8bit256 <64986275+8bit256@users.noreply.github.com>
2022-02-11 12:58:27 +0000

    02/11/2022 @ 12:57pm

---
+++ 2. GCSE/2022 (Y11)/Programming Tools/Subroutine/function.py
@@ -0,0 +1,12 @@
+# function addTwoNums
+# a function is a named groups of statements / instruction that must return a value
+
+def addTwoNums(): # defined the function
+    num1 = 1
+    num2 = 32
+    sum = num1 + num2
+    return sum # return the value to the calling code
+
+total = addTwoNums() # this is the calling code / usage
+
+print(total)
