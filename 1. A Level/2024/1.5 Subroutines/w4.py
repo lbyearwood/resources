0d355cbf34ae807e69d2cb36d9c4192c35223a5f
8bit256 <64986275+8bit256@users.noreply.github.com>
2022-09-30 12:55:57 +0100

    -

--- 1. A Level/2024/1.5 Subroutines/w4.py
+++ 1. A Level/2024/1.5 Subroutines/w4.py
@@ -29,18 +29,34 @@ def menu():
             print("Please choose a value option between 1 and 5")
 
 
-def emptyCarPark():
-    pass
+def emptyCarPark(): # complete
+    global carPark
+    carPark = [["empty" for cols in range(25)] for rows in range(20)]
 
 def parkACar():
-    pass
+    reg = input("Please enter your car reg")
+    while True:  # infinite loop
+        row = int(input("Please enter a row between 1 and 20"))
+        col = int(input("Please enter a column between 1 and 25"))
+        if (row >= 1 and row <= 20) and (col >= 1 and col <= 25):
+            # subtract 1 to handle array index
+            col -= 1
+            row -= 1
+            if carPark[row][col] == "empty":
+                carPark[row][col] = reg
+                print("Car park space booked")
+                break
+            else:
+                print("Space taken! Please select another space")
+        else:
+            print("Please enter a valid row and column")
 
 def removeACar():
     pass
 
-def displayCarParkGrid():
+def displayCarParkGrid(): # complete
     for row in carPark:
         print(row)  # print the row
 
-
-menu()
+while True:
+    menu()
