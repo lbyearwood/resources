b28cf884f08b0898ce0405afa3e05cdddf548bfa
8bit256 <64986275+8bit256@users.noreply.github.com>
2022-10-14 13:32:34 +0100

    -

--- 1. A Level/2024/1.6 File Handling and Exception Handling/Lesson examples/Exception Handling.py
+++ 1. A Level/2024/1.6 File Handling and Exception Handling/Lesson examples/Exception Handling.py
@@ -1,4 +1,9 @@
-file = open("test2.txt","r")
-x = file.read()
+try:
+    file = open("test2.txt","r")
+    x = file.read()
+    print(x)
+except Exception as e:
+    print(e)
 
-print(x)
+print("Proof")
+
