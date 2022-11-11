b4a335ab9688d36675588bc4aa4b1f01bbd09d57
8bit256 <64986275+8bit256@users.noreply.github.com>
2022-11-11 12:44:01 +0000

    _

--- 1. A Level/2024/2.3 Interpreting Algorithms/anagram.py
+++ 1. A Level/2024/2.3 Interpreting Algorithms/anagram.py
@@ -8,22 +8,33 @@ def generate_word(dictionary):
     return dictionary[r.randint(0,len(dictionary)-1)]
 
 def main(): # call everything from this subroutine
+    listOfUsedAnagrams = []
     score = 0
     dictionary = generate_dictionary()
     generatedWord = generate_word(dictionary)
     print(f"Create an anagram from {generatedWord}")
     while True:
-        userWord = input("Enter your anagram")
+        userWord = input("Enter your anagram").strip()
         validWord = isValidWord(userWord,dictionary)
-        if validWord:
+        if validWord and not(isUnsed(userWord,listOfUsedAnagrams)):
             validAnagram = isAnagram(userWord,generatedWord)
             if validAnagram:
                 score+=1
-                print(f"{userWord} is correct\nYour score is now{score}")
+                print(f"{userWord} is correct\nYour score is now {score}")
+                listOfUsedAnagrams.append(userWord) # add the anagram to the used list
             else:
                 print(f"{userWord} is not an anagram")
         else:
             print(f"{userWord} is not a real word")
+
+def isUnsed(userWord,listOfUsedAnagrams):
+    if userWord in listOfUsedAnagrams:
+        print("Error, you have alreay used this word")
+        return True
+    else:
+        return False
+
+
 
 def isAnagram(userWord,generatedWord): # check if the user's word is an anagram
     if len(userWord) == 0: # validate input
